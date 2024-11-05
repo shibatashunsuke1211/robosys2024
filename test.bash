f73ca9f54784c3855f92e8a928d674f8f8e5ed7f
ShunsukeShibata <s23C1068BM@s.chibakoudai.jp>
2024-11-05 20:31:10 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,14 +1,36 @@
-#!/bin/bash
+#!/bin/bash -xv
+#SPDX-FileCopyrightText: 2024 Shunsuke Shibata
+# SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
 	echo ${1}行目が違うよ 
-	res=1                   #追加
+	res=1                   
 }
 
 res=0
-a=山田
-[ "$a" = 上田 ] || ng "$LINENO"
-[ "$a" = 山田 ] || ng "$LINENO"
 
+out=$(seq 5 | ./plus)
+++seq 5
+++ ./plus
++ out=15
+[ "${out}" = 15 ] || "$LINENO"
++'[' 15 = 15 ']'
+
+[ "${res}" = 0 ] && echo OK
++ '['0 = 0']'
++ echo OK
+OK
+exit $res
++ exit 0
+
+out=$(echo あ | ./plus)
+["$?" = 1]
+["${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]
+[ "${out}" = "" ] || ng "$LINENO"
+
+[ "$res" = 0 ] && echo OK
 exit $res
 
